13b342ecb4cf1b3ab816f59f56acc8181050a866
Jose <jfmcode@gmail.com>
2016-03-13 16:09:49 +0000

    Create scripts for CLI
    Delete normalize script and add normalize method to module.
    Create CLI script for check and normalize files.
    [Time: 1h]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Load dependencies.
 const fs = require('fs');
 const unzip = require('unzip');
+const stripBOM = require('strip-bom');
 const Dictionary = require('./dictionary.js');
 
 const FOLDER_PATH = './dict';
@@ -14,8 +15,8 @@ var SpellChecker = {
      * @param {String} fileName The name of the file from which read the word list.
      * @param {String} folderPath The path to the directory in which the file is located (optional).
      * @param {Callback} callback A function to invoke when either the dictionary was created or an error was found.
-     */  
-    getDictionary: function(fileName, folderPath) {
+     */
+    getDictionary: function(fileName, folderPath /*, callback*/) {
         try{
             // Initialize variables.
             var folder = (!folderPath || typeof folderPath != 'string')? FOLDER_PATH : folderPath;
@@ -39,7 +40,7 @@ var SpellChecker = {
                             });
                         } else {
                             // The ZIP file also doesn't exists, return an error.
-                            callback('The dicctionary could not be created, no file with the name "' + fileName + '" could be found', null);
+                            callback('The dicctionary could not be read, no file with the name "' + fileName + '" could be found', null);
                         } 
                     });
                 }
@@ -67,7 +68,6 @@ var SpellChecker = {
                 // Return an error.
                 callback("The dictionary file could not be read: " + err, null);
             }
-          console.log(data);
         });
     },
   
@@ -99,6 +99,67 @@ var SpellChecker = {
         } catch(err) {
             // Throw an error.
             throw new Error('An unexpected error ocurred: ' + err);
+        }
+    },
+    
+    /**
+     * Reads a UTF8 dictionary file, removes the BOM and \r characters and sorts the list of words.
+     *
+     * @param {String} inputPath The path for the input file.
+     * @param {String} outputPath The path to output (optional, by default is equals to the input file).
+     * @param {Callback} callback A function to invoke after finishing.
+     */
+    normalizeDictionary: function(inputPath, outputPath /*, callback*/) {
+        try{
+            // Parses arguments
+            if(!outputPath || typeof outputPath != 'string') outputPath = inputPath;
+            var callback = arguments.length > 0? arguments[arguments.length - 1] : function() {};
+            
+            // Verify if the dictionary file exists.
+            fs.exists(inputPath, function(exists) {
+                if(exists) {
+                    // The file exists, read it.
+                    fs.readFile(inputPath, 'utf8', function(err, content) {
+                        // Check for errors.
+                        if (!err) {
+                            // Remove BOM and \r characters.
+                            content = stripBOM(content);
+                            content = content.replace(/\r/g, '');
+                            
+                            // Sort words.
+                            var lines = content.split('\n');      
+                            var collator = new Intl.Collator(); // Use this comparator for consider accents and special characters.
+                            lines = lines.sort(collator.compare);
+                            
+                            // Generate output content.
+                            var newContent = '';  
+                            var first = true;
+                            for(var i=0; i<lines.length; i++) {          
+                                if(lines[i] != '' && lines[i] != '\n') {
+                                    if(!first) newContent += '\n';
+                                    newContent += lines[i];
+                                    first = false;
+                                }
+                            }
+                            
+                            // Write output file.
+                            fs.writeFile(outputPath, newContent, 'utf8', function(err) {
+                                // Return result.
+                                callback(err? ("The output file could not be writted: " + err) : null, !err);
+                            });
+                        } else {
+                            // Return an error.
+                            callback("The input file could not be read: " + err, false);
+                        }
+                    });
+                } else {
+                    // Return an error indicating that the file doens't exists.
+                    callback("The input file does not exists", false);
+                }            
+            });
+        } catch(err) {
+            // Return an error.
+            callback('An unexpected error ocurred: ' + err, false);
         }
     }
 }
