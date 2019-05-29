3d20f82f84b4274c43189f6ed5f8dd058866f172
Jacob Samro <jacobsamro@outlook.com>
2019-05-30 05:21:06 +0530

    Fix: few linting errors

--- src/services/spellchecker.service.ts
+++ src/services/spellchecker.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
 
-import { Dictionary } from './dictionary'
+import { Dictionary } from './dictionary';
 
 @Injectable()
 export class SpellCheckerService {
